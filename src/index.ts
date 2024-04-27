861fba40f8d931c56454ca3101c9ab56d7fe63b0
JesusMartinezClavel <jmcvalles@gmail.com>
2024-04-27 18:23:38 +0200

    INIT -> Installed project's dependencies || created github's repository

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Express, { Request, Response } from "express";
+
+const app = Express()
+
+app.listen(3000, () => console.log('server connected on 3000'))
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world')
+})
