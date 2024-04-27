a76f640059d005dee4280059781f3475c4401a53
JesusMartinezClavel <jmcvalles@gmail.com>
2024-04-27 18:51:22 +0200

    FEAT -> Installed typeOrm, reflect-metadata and mysql2 dependencies

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const app = Express()
 app.listen(3000, () => console.log('server connected on 3000'))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello world')
+    res.send('hello puta madre')
 })
