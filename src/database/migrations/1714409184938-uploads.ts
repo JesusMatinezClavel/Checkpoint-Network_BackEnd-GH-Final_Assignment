bca5838dca4814f798dba51cda170a548956c9de
JesusMartinezClavel <jmcvalles@gmail.com>
2024-04-29 19:28:29 +0200

    FEAT -> Created migration: uploads

--- src/database/migrations/1714409184938-uploads.ts
+++ src/database/migrations/1714409184938-uploads.ts
@@ -1,8 +1,63 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Uploads1714409184938 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'uploads',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment'
+                    }, {
+                        name: 'name',
+                        type: 'varchar',
+                        length: '255',
+                        isNullable: false
+                    }, {
+                        name: 'path',
+                        type: 'varchar',
+                        length: '500',
+                        isNullable: false
+                    }, {
+                        name: 'description',
+                        type: 'text'
+                    },
+                    {
+                        name: 'downloadable',
+                        type: 'boolean',
+                        default: false
+                    },
+                    {
+                        name: 'createdAt',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updatedAt',
+                        type: 'timestamp',
+                        default: 'now()',
+                        onUpdate: 'now()'
+                    },
+                    {
+                        name: 'author',
+                        type: 'int'
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ['author'],
+                        referencedTableName: 'users',
+                        referencedColumnNames: ['id'],
+                        onDelete: 'CASCADE'
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
