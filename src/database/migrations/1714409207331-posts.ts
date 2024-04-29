04caa7bb78b57251204a87462b56ce1e01d5365b
JesusMartinezClavel <jmcvalles@gmail.com>
2024-04-29 19:35:53 +0200

    FEAT -> Created migration: posts

--- src/database/migrations/1714409207331-posts.ts
+++ src/database/migrations/1714409207331-posts.ts
@@ -1,8 +1,55 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Posts1714409207331 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'posts',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment'
+                    },
+                    {
+                        name: 'title',
+                        type: 'varchar',
+                        length: '255',
+                        isNullable: false
+                    },
+                    {
+                        name: 'description',
+                        type: 'text',
+                    },
+                    {
+                        name: 'createdAt',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updatedAt',
+                        type: 'timestamp',
+                        default: 'now()',
+                        onUpdate: 'now()'
+                    },
+                    {
+                        name: 'upload_id',
+                        type: 'int'
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ['upload_id'],
+                        referencedTableName: 'uploads',
+                        referencedColumnNames: ['id'],
+                        onDelete: 'CASCADE'
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
