0fee4f169a55d4c8e391ef999e1361fca331d828
JesusMartinezClavel <jmcvalles@gmail.com>
2024-05-09 14:27:17 +0200

    FEAT -> Created router for registerModel

--- src/entities/file/file.routes.ts
+++ src/entities/file/file.routes.ts
@@ -8,9 +8,10 @@ import { auth } from "../../middlewares/auth-middleware";
 // Instances
 const router = Router()
 const uploadAvatar = multer({ dest: 'img/avatars' })
+const uploadModel = multer({ dest: '3D-models' })
 
 router.post('/avatar', uploadAvatar.single('avatar'), registerAvatar)
-router.post('/upload', auth, registerUpload)
+router.post('/upload', uploadModel.single('uploadFile'), auth, registerUpload)
 router.get('/avatar/:id', auth, getAvatar)
 router.get('/download/:id', getUploadFile)
 
