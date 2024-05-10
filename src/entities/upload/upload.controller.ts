9a8f16d929080645b487d9bbfda613d406020ff3
JesusMartinezClavel <jmcvalles@gmail.com>
2024-05-10 21:04:42 +0200

    FEAT -> Created deleteUpload controller

--- src/entities/upload/upload.controller.ts
+++ src/entities/upload/upload.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { catchStatus, tryStatus } from "../../utils/resStatus";
 import { Upload } from "./Upload";
+import { User } from "../user/User";
 import fs from "fs";
 import path from "path";
 import { ReadStream } from 'fs';
@@ -41,6 +42,15 @@ export const createUpload = async (req: Request, res: Response) => {
         const { name, description, downloadable } = req.body
         const userId = req.tokenData.userId
 
+        const user = await User.findOne({
+            where: {
+                id: userId
+            }
+        })
+
+        if (!user) {
+            throw new Error('user doesnt exists')
+        }
         if (!name) {
             throw new Error('required fields')
         }
@@ -69,6 +79,55 @@ export const createUpload = async (req: Request, res: Response) => {
                 case error.message.includes('invalid file'):
                     statusCode = 402
                     errorMessage = 'You need valid file!'
+                    break;
+                default:
+                    break;
+            }
+        catchStatus(res, statusCode, 'CANNOT CREATE UPLOAD', new Error(errorMessage))
+    }
+}
+
+export const deleteUpload = async (req: Request, res: Response) => {
+    try {
+        const userId = req.tokenData.userId
+        const uploadId = Number(req.params.id)
+
+        const user = await User.findOne({
+            where: {
+                id: userId
+            }
+        })
+
+        if (!user) {
+            throw new Error('user doesnt exists')
+        }
+
+        const upload = await Upload.findOne({
+            where: {
+                id: uploadId
+            }
+        })
+
+        if (!upload) {
+            throw new Error('upload doesnt exists')
+        }
+
+        await Upload.delete(uploadId)
+
+        tryStatus(res, 'Upload deleted!', null)
+    } catch (error) {
+        let statusCode: number = 500
+        let errorMessage: string = 'Unkown error ocurred...'
+
+        if (error instanceof Error)
+            switch (true) {
+                case error.message.includes('user doesnt exists'):
+                    statusCode = 409
+                    errorMessage = "User doesn't exists"
+                    break;
+                case error.message.includes('upload doesnt exists'):
+                    statusCode = 409
+                    errorMessage = "Upload doesn't exists"
                     break;
                 default:
                     break;
